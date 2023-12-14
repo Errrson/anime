719fba8964d75c77dfb15ff55baec5b02660a203
Errrson <el.circuitos.jeff@gmail.com>
2023-12-14 20:10:27 +0100

    Se crea la segunda ruta para la pagina de detalles de cada anime. Y su template

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello_world():
     return render_template("index.html")
 
 
+@app.route("/anime/<string:name_anime>")
+def page_anime(name_anime):
+    return render_template("anime.html", name_anime=name_anime)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
